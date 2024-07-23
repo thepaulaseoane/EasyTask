69f4690e4716d70d77cac2c79974168f5537b9b1
Paula Seoane <paulaseoaner@gmail.com>
2024-07-23 20:35:25 +0200

    feat: dynamic user info

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
     selector: 'app-user',
@@ -7,4 +10,6 @@ import { Component } from "@angular/core";
     styleUrl: './user.component.css'
 })
 
-export class UserComponent {}
+export class UserComponent {
+    selectedUser = DUMMY_USERS[randomIndex];
+}
